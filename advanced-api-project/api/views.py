3b4663c6ec23574263a67318ffa9ec1775e84602
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-08-05 23:09:26 +0100

    Add generic views and permissions for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# List all books (GET) or create a new one (POST)
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Allow anyone to view, but only logged-in users to create
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+# Retrieve, update, or delete a book by ID
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Same permission setup
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
