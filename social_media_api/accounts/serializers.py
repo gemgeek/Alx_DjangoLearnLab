2e901e1a2868aaab754edf103b3a3f1191419b8b
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-08-22 14:56:34 +0100

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,18 +4,17 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # <-- explicit CharField
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'confirm_password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password']
-        )
-        Token.objects.create(user=user)  # <-- explicit Token creation
+        password = validated_data.pop('password')
+        validated_data.pop('confirm_password', None)
+        user = get_user_model().objects.create_user(**validated_data, password=password)
+        Token.objects.create(user=user)
         return user
